82ca304f3da4d208505d0d1e9758c987955630c0
Satheesh Reddy <Satheesh Reddy@192.168.0.2>
2020-10-07 20:33:58 +0530

    restfullservices

---
+++ src/main/java/classic350/EmployeePOJO.java
@@ -0,0 +1,10 @@
+package classic350;
+
+public class EmployeePOJO {
+
+	public int id;
+	public String employee_name;
+	public long employee_salary;
+	public int employee_age;
+	public String profile_image;
+   }
