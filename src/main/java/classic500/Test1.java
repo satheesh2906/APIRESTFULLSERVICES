803dfe10e9a8bfcde977bd6b75e1e3dca0bf7603
Satheesh Reddy <Satheesh Reddy@192.168.0.2>
2020-10-07 23:08:01 +0530

    test1

--- src/main/java/classic500/Test1.java
+++ src/main/java/classic500/Test1.java
@@ -2,8 +2,9 @@ package classic500;
 
 public class Test1 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
+	public static void main(String[] args) 
+	{
+		
 
 	}
 
